a6e815ebcfc7b4a08d146092183ec866f17a45f4
dcendry1711 <daniel.cendry@gmail.com>
2026-05-10 15:23:42 +0200

    Update userLifecycle.flow.ts

--- flows/userLifecycle.flow.ts
+++ flows/userLifecycle.flow.ts
@@ -1,15 +1,18 @@
 import { Page, expect} from "@playwright/test"
 import { Navbar } from "../components/navigation.component"
+import { HomePage } from "../pages/home.page";
 import { LoginPage } from "../pages/login.page";
 import { SignupPage } from "../pages/signup.page";
 import { AccCreatedPage } from "../pages/accCreated.page";
 import { AccDeletedPage } from "../pages/accDeleted.page";
 import { generateNewUserData } from "../utils/generateNewUserData.util";
 import { userAccInfoForm } from "../data/userAccInfoForm.data";
+import { usersArr } from "../utils/generateNewUserData.util";
 
 export class UserLifecycle{
 
     navbar: Navbar;
+    homePage: HomePage;
     loginPage: LoginPage;
     signupPage: SignupPage;
     accCreatedPage: AccCreatedPage;
@@ -19,20 +22,20 @@ export class UserLifecycle{
     async registerNewUser(){
 
         //generate new user name and email
-        const userData = generateNewUserData()
+        generateNewUserData()
         //navigate to login page
         await this.navbar.signupLoginLink.click();
 
         //verify display of login page, fill new user registration form and proceed
         await expect(this.loginPage.newUserSignupHeader).toBeVisible();
-        await this.loginPage.newUserName.fill(userData.userName);
-        await this.loginPage.newUserEmail.fill(userData.userEmail);
+        await this.loginPage.newUserName.fill(usersArr[0].userName);
+        await this.loginPage.newUserEmail.fill(usersArr[0].userEmail);
         await this.loginPage.signupBtn.click();
 
         //verify display of signup page, fill account and address information form -> create account
         await expect(this.signupPage.enterAccInfHeader).toBeVisible();
         await this.signupPage.mrTitle.check();
-        await this.signupPage.accountPassword.fill(userAccInfoForm.password);
+        await this.signupPage.accountPassword.fill(usersArr[0].userPassword);
         await this.signupPage.dayOfBirth.selectOption(userAccInfoForm.dayOfBirth);
         await this.signupPage.monthOfBirth.selectOption(userAccInfoForm.monthOfBirth);
         await this.signupPage.yearOfBirth.selectOption(userAccInfoForm.yearOfBirth);
@@ -55,7 +58,7 @@ export class UserLifecycle{
         await this.accCreatedPage.continueBtn.click();
 
         //verify of logged user in app
-        await expect(this.navbar.loggedInUser).toContainText(userData.userName)
+        await expect(this.navbar.loggedInUser).toContainText(usersArr[0].userName)
     }
 
     //asynchrous function - delete user account 
@@ -65,8 +68,24 @@ export class UserLifecycle{
         await this.accDeletedPage.continueBtn.click();
     }
 
+    async logoutUser(){
+        await this.navbar.logoutLink.click();
+        await expect(this.navbar.loggedInUser).toBeHidden();
+        await expect(this.homePage.pageHeader).toBeVisible();
+    }
+
+    async loginUser(){
+        await this.navbar.signupLoginLink.click();
+        await expect(this.loginPage.loginUserHeader).toBeVisible();
+        await this.loginPage.loginEmail.fill(usersArr[0].userEmail);
+        await this.loginPage.loginPassword.fill(usersArr[0].userPassword);
+        await this.loginPage.loginBtn.click();
+        await expect(this.navbar.loggedInUser).toContainText(usersArr[0].userName);
+    }
+
     constructor(private page: Page){
         this.navbar = new Navbar(page);
+        this.homePage = new HomePage(page);
         this.loginPage = new LoginPage(page);
         this.signupPage = new SignupPage(page);
         this.accCreatedPage = new AccCreatedPage(page);

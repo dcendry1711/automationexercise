20a0825d58fd5b0be9d83a81b9c38387c4d6f83b
dcendry1711 <daniel.cendry@gmail.com>
2026-05-10 12:27:21 +0200

    Create userAccInfoForm.data.ts

---
+++ data/userAccInfoForm.data.ts
@@ -0,0 +1,16 @@
+export const userAccInfoForm = {
+    password: '1234',
+    dayOfBirth: '1',
+    monthOfBirth: '1',
+    yearOfBirth: '2000',
+    firstName: 'TestFirstName',
+    lastName: 'TestLastName',
+    company: 'TestCompany',
+    address: 'TestAddress',
+    address2: 'TestAddress2',
+    country: 'United States',
+    state: 'TestState',
+    city: 'TestCity',
+    zipcode: '00001',
+    mobileNumber: '+48 111-111-111'
+}

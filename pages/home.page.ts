85cd584073cfa21a932b7a47b81e083330d2c235
dcendry1711 <daniel.cendry@gmail.com>
2026-05-13 16:49:00 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,12 +1,26 @@
-import { Locator, Page } from "@playwright/test"
+import { Locator, Page, expect } from "@playwright/test"
 
 export class HomePage{
 
     consentBtn: Locator;
     pageHeader: Locator;
+    footerSubscriptionHeader: Locator;
+    subscriptionEmailInput: Locator;
+    submitSubscriptionBtn: Locator;
+    successSubscriptionMsg: Locator;
+
+    async submitSubscriptionWithVerification(){
+        await this.subscriptionEmailInput.fill('test@test.com');
+        await this.submitSubscriptionBtn.click();
+        await expect(this.successSubscriptionMsg).toContainText('You have been successfully subscribed!');
+    }
 
     constructor(private page: Page){
         this.consentBtn = page.locator('p.fc-button-label', {hasText: 'Consent'});
         this.pageHeader = page.locator('div').first();
+        this.footerSubscriptionHeader = page.locator('.single-widget h2');
+        this.subscriptionEmailInput = page.locator('#susbscribe_email');
+        this.submitSubscriptionBtn = page.locator('#subscribe');
+        this.successSubscriptionMsg = page.getByText('You have been successfully');
     }
 }

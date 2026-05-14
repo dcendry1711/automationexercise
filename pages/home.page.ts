c848201f5a7e99c8e92cf7b3a8425e3b5b4fdf2f
dcendry1711 <daniel.cendry@gmail.com>
2026-05-14 12:58:57 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,26 +1,15 @@
 import { Locator, Page, expect } from "@playwright/test"
+import { Footer } from "../components/footer.component";
 
 export class HomePage{
 
+    footer: Footer;
     consentBtn: Locator;
     pageHeader: Locator;
-    footerSubscriptionHeader: Locator;
-    subscriptionEmailInput: Locator;
-    submitSubscriptionBtn: Locator;
-    successSubscriptionMsg: Locator;
-
-    async submitSubscriptionWithVerification(){
-        await this.subscriptionEmailInput.fill('test@test.com');
-        await this.submitSubscriptionBtn.click();
-        await expect(this.successSubscriptionMsg).toContainText('You have been successfully subscribed!');
-    }
 
     constructor(private page: Page){
+        this.footer = new Footer(page);
         this.consentBtn = page.locator('p.fc-button-label', {hasText: 'Consent'});
         this.pageHeader = page.locator('div').first();
-        this.footerSubscriptionHeader = page.locator('.single-widget h2');
-        this.subscriptionEmailInput = page.locator('#susbscribe_email');
-        this.submitSubscriptionBtn = page.locator('#subscribe');
-        this.successSubscriptionMsg = page.getByText('You have been successfully');
     }
 }

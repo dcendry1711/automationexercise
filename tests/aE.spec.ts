fb56ede4d1e74ed73f5cb7d9b12feb19e1bb28ab
dcendry1711 <daniel.cendry@gmail.com>
2026-05-17 16:24:26 +0200

    Update aE.spec.ts

--- tests/aE.spec.ts
+++ tests/aE.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "../fixtures/aC.fixture"
 import { GoToHomePage } from "../flows/goToHomePage.flow";
 import { UserLifecycle } from "../flows/userLifecycle.flow";
 import { SubmitContactForm } from "../flows/submitContactForm.flow";
+import { userAccInfoForm } from "../data/userAccInfoForm.data";
 
 test.describe("Automation exercise test cases", () => {
 
@@ -11,30 +12,38 @@ test.describe("Automation exercise test cases", () => {
         await goToHomePage.loadAndVerifyContent();
     })
 
-    test("TC01 - Register User", async({ page }) => {
+    test("TC01 - Register User", async({ page, navbar }) => {
         const userLifecycle = new UserLifecycle(page);
+        //navigate to login page
+        await navbar.signupLoginLink.click();
         //register new user - full process with validation of logged user
         await userLifecycle.registerNewUser();
     })
 
-    test("TC02 - Register User and delete registered user account", async({ page }) => {
+    test("TC02 - Register User and delete registered user account", async({ page, navbar }) => {
         const userLifecycle = new UserLifecycle(page);
+        //navigate to login page
+        await navbar.signupLoginLink.click();
         //register new user - full process with validation of logged user
         await userLifecycle.registerNewUser();
         //delete logged user account with verification after finished action 
         await userLifecycle.deleteUserAcc();
     })
 
-    test("TC03 - Logout user", async({ page }) => {
-        const userLifecycle = new UserLifecycle(page)
+    test("TC03 - Logout user", async({ page, navbar }) => {
+        const userLifecycle = new UserLifecycle(page);
+        //navigate to login page
+        await navbar.signupLoginLink.click();
         //register new user for test scenario
         await userLifecycle.registerNewUser();
         //logout after register user
         await userLifecycle.logoutUser();
     })
 
-    test("TC04 - Login User with correct email and password", async({ page }) => {
-        const userLifecycle = new UserLifecycle(page)
+    test("TC04 - Login User with correct email and password", async({ page, navbar }) => {
+        const userLifecycle = new UserLifecycle(page);
+        //navigate to login page
+        await navbar.signupLoginLink.click();
         //register new user for test scenario
         await userLifecycle.registerNewUser();
         //logout after register user
@@ -44,7 +53,7 @@ test.describe("Automation exercise test cases", () => {
     })
 
     test("TC05 - Login user with incorrect email and password", async({ page }) =>{
-        const userLifecycle = new UserLifecycle(page)
+        const userLifecycle = new UserLifecycle(page);
         //go to login page and verify display of login page
         await userLifecycle.navigateToLoginPage();
         //login with incorrect user data and verify error message display
@@ -104,11 +113,11 @@ test.describe("Automation exercise test cases", () => {
         //navigate to products page
         await navbar.productsLink.click();
         //add 1st product on list to cart
-        await productsPage.addProductToCart("1");
+        await productsPage.addProductToCartOnProductsPage("1");
         //continue shopping
         await productsPage.purchaseModal.continueShoppingLinkModal.click();
         //add 2nd product on list to cart
-        await productsPage.addProductToCart("2");
+        await productsPage.addProductToCartOnProductsPage("2");
         //go to cart page
         await productsPage.purchaseModal.viewCartLinkOnModal.click();
         //verify products added to cart
@@ -126,5 +135,44 @@ test.describe("Automation exercise test cases", () => {
         await cartPage.verifyProductQuantityInCart(1,'4');
     })
 
-    
+    test("TC14 - Place Order: Register while Checkout", async({ page, navbar, homePage, cartPage, checkoutPage, paymentPage, paymentDonePage }) => {
+        const userLifecycle = new UserLifecycle(page);
+        //add products to cart on home page
+        await homePage.addProductToCartOnHomePage("1");
+        await homePage.purchaseModal.continueShoppingLinkModal.click();
+        await homePage.addProductToCartOnHomePage("2");
+        await homePage.purchaseModal.continueShoppingLinkModal.click();
+        await navbar.cartLink.click();
+        expect(page.url()).toContain('/view_cart');
+        await cartPage.proceedToCheckoutBtn.click();
+        await cartPage.modal.registerLoginLinkOnModal.click();
+        await userLifecycle.registerNewUser();
+        await navbar.cartLink.click();
+        await cartPage.proceedToCheckoutBtn.click();
+        //verify address details and review user order
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.firstName);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.lastName);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.address);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.address2);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.city);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.state);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.zipcode);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.country);
+        expect(checkoutPage.deliveryAddressBlock).toContainText(userAccInfoForm.mobileNumber);
+        await checkoutPage.rewiev1stProduct();
+        await checkoutPage.review2ndProduct();
+        //enter description in comment text area and click place order button
+        await checkoutPage.commentOrderTextArea.fill('test comment');
+        await checkoutPage.placeOrderBtn.click();
+        //enter payment details
+        await paymentPage.nameOnCard.fill(`${userAccInfoForm.firstName} ${userAccInfoForm.lastName}`);
+        await paymentPage.cardNumber.fill('1111111111111111');
+        await paymentPage.cvc.fill('111');
+        await paymentPage.expirationMonth.fill('12');
+        await paymentPage.expirationYear.fill('2099');
+        await paymentPage.payAndConfirmBtn.click();
+        //Verify success message 'Your order has been placed successfully!'
+        await expect(paymentDonePage.successMsg).toContainText('Congratulations! Your order has been confirmed!');
+        await userLifecycle.deleteUserAcc();
+    })
 })

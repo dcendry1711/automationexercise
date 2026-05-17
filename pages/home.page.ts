c540db9f8f66260f07504de20ee7f3d17bcc8d84
dcendry1711 <daniel.cendry@gmail.com>
2026-05-17 14:29:10 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -7,6 +7,16 @@ export class HomePage{
     consentBtn: Locator;
     pageHeader: Locator;
 
+    getProductLocatorOnHomePage(productNumber: string){
+        return this.page.locator(`a[href="/product_details/${productNumber}"]`)
+    }
+
+    async viewProductDetailsOnHomePage(productNumber: string){
+        const selectedProduct = this.getProductLocatorOnHomePage(productNumber);
+        await selectedProduct.click();
+        expect(this.page.url()).toContain(`/product_details/${productNumber}`);
+    }
+
     constructor(private page: Page){
         this.footer = new Footer(page);
         this.consentBtn = page.locator('p.fc-button-label', {hasText: 'Consent'});

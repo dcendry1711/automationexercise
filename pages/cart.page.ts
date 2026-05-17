1568dc1a9369f43fde49431c6383b03be89ebf3c
dcendry1711 <daniel.cendry@gmail.com>
2026-05-17 19:31:52 +0200

    Update cart.page.ts

--- pages/cart.page.ts
+++ pages/cart.page.ts
@@ -1,6 +1,8 @@
 import { Locator, Page, expect } from "@playwright/test"
 import { Footer } from "../components/footer.component"
 import { Modal } from "../components/modal.component";
+import { Product } from "../types/product";
+
 export class CartPage{
 
     footer: Footer;
@@ -13,20 +15,17 @@ export class CartPage{
         return this.page.locator(`#product-${rowNumber}`);
     }
 
-    async verify1stProductInCart(){
-        const cart1ProductRow = this.getCartProductRow(1);
-        await expect(cart1ProductRow.locator('.cart_description a')).toContainText('Blue Top');
-        await expect(cart1ProductRow.locator('.cart_price p')).toContainText('Rs. 500');
-        await expect(cart1ProductRow.locator('.cart_quantity button')).toContainText('1');
-        await expect(cart1ProductRow.locator('.cart_total p')).toContainText('Rs. 500');
-    }
-
-    async verify2ndProductInCart(){
-        const cart2ProductRow = this.getCartProductRow(2);
-        await expect(cart2ProductRow.locator('.cart_description a')).toContainText('Men Tshirt');
-        await expect(cart2ProductRow.locator('.cart_price p')).toContainText('Rs. 400');
-        await expect(cart2ProductRow.locator('.cart_quantity button')).toContainText('1');
-        await expect(cart2ProductRow.locator('.cart_total p')).toContainText('Rs. 400');
+    async verifyProductsInCart(products: Product[]){
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i];
+            const rowNumber = i + 1;
+            const cartProductRow = this.getCartProductRow(rowNumber);
+            
+            await expect(cartProductRow.locator('.cart_description a')).toContainText(product.name);
+            await expect(cartProductRow.locator('.cart_price p')).toContainText(product.price);
+            await expect(cartProductRow.locator('.cart_quantity button')).toContainText(product.quantity);
+            // Można również dodać weryfikację sumy, jeśli jest wymagana
+        }
     }
 
     async verifyProductQuantityInCart(row: number, expectedQuantity: string){

4f34a251abaf44c4e58dab376b57252cbacbde0f
dcendry1711 <daniel.cendry@gmail.com>
2026-05-17 14:29:12 +0200

    Update productDetails.page.ts

--- pages/productDetails.page.ts
+++ pages/productDetails.page.ts
@@ -1,10 +1,14 @@
 import { Locator, Page, expect } from "@playwright/test"
+import { PurchaseModal } from "../components/purchaseModal.component";
 
 export class ProductDetailsPage {
 
+    purchaseModal: PurchaseModal;
     productName: Locator;
     productCategory: Locator;
     productPrice: Locator;
+    productQuantity: Locator;
+    addToCartBtn: Locator;
     productAvailability: Locator;
     productCondition: Locator;
     productBrand: Locator;
@@ -18,10 +22,18 @@ export class ProductDetailsPage {
         await expect(this.productBrand).toBeVisible();
     }
 
+    async specifyQuantityAndOrderProduct(quantity: string){
+        await this.productQuantity.fill(quantity);
+        await this.addToCartBtn.click();
+    }
+
     constructor(private page: Page) {
+        this.purchaseModal = new PurchaseModal(page);
         this.productName = page.getByRole('heading', { name: 'Blue Top' });
         this.productCategory = page.getByText('Category: Women > Tops');
         this.productPrice = page.getByText('Rs.');
+        this.productQuantity = page.locator('#quantity');
+        this.addToCartBtn = page.locator('.product-information button');
         this.productAvailability = page.getByText('Availability:')
         this.productCondition = page.getByText('Condition:');
         this.productBrand = page.getByText('Brand:');

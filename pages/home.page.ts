e14986475aad08adbf6edf224531b525d3574bf8
dcendry1711 <daniel.cendry@gmail.com>
2026-05-17 16:24:14 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,23 +1,35 @@
 import { Locator, Page, expect } from "@playwright/test"
 import { Footer } from "../components/footer.component";
+import { Modal } from "../components/modal.component";
 
 export class HomePage{
 
+    purchaseModal: Modal;
     footer: Footer;
     consentBtn: Locator;
     pageHeader: Locator;
 
-    getProductLocatorOnHomePage(productNumber: string){
+    getProductDetailsLocatorOnHomePage(productNumber: string){
         return this.page.locator(`a[href="/product_details/${productNumber}"]`)
     }
 
     async viewProductDetailsOnHomePage(productNumber: string){
-        const selectedProduct = this.getProductLocatorOnHomePage(productNumber);
+        const selectedProduct = this.getProductDetailsLocatorOnHomePage(productNumber);
         await selectedProduct.click();
         expect(this.page.url()).toContain(`/product_details/${productNumber}`);
     }
 
+    getProductLocatorToOrderOnHomePage(productId: string){
+        return this.page.locator(`a[data-product-id="${productId}"]`);
+    }
+
+    async addProductToCartOnHomePage(productId: string){
+        const prodLocator = this.getProductLocatorToOrderOnHomePage(productId).first();
+        await prodLocator.click();
+    }
+
     constructor(private page: Page){
+        this.purchaseModal = new Modal(page);
         this.footer = new Footer(page);
         this.consentBtn = page.locator('p.fc-button-label', {hasText: 'Consent'});
         this.pageHeader = page.locator('div').first();

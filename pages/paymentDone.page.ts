c1aefea9b3a3160f739d8c16b4434e6b234e4f13
dcendry1711 <daniel.cendry@gmail.com>
2026-05-18 16:43:06 +0200

    Update paymentDone.page.ts

--- pages/paymentDone.page.ts
+++ pages/paymentDone.page.ts
@@ -1,8 +1,12 @@
-import { Locator, Page } from "@playwright/test"
+import { Locator, Page, expect } from "@playwright/test"
 
 export class PaymentDonePage{
 
     successMsg: Locator;
+
+    async verifyDisplaySuccesMsg(){
+        await expect(this.successMsg).toContainText('Congratulations! Your order has been confirmed!');
+    }
 
     constructor(private page: Page){
         this.successMsg = page.getByText('Congratulations! Your order');

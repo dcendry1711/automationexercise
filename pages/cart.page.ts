fac0ce70d55365002edec25d34ed35d7c50941df
dcendry1711 <daniel.cendry@gmail.com>
2026-05-19 15:05:40 +0200

    Update cart.page.ts

--- pages/cart.page.ts
+++ pages/cart.page.ts
@@ -24,13 +24,26 @@ export class CartPage{
             await expect(cartProductRow.locator('.cart_description a')).toContainText(product.name);
             await expect(cartProductRow.locator('.cart_price p')).toContainText(product.price);
             await expect(cartProductRow.locator('.cart_quantity button')).toContainText(product.quantity);
-            // Można również dodać weryfikację sumy, jeśli jest wymagana
         }
+    }
+
+    async verifyProductsInCartAfterRemove(products: Product[]){
+        const product = products
+        const cartProductRow = this.getCartProductRow(1);
+
+        await expect(cartProductRow.locator('.cart_description a')).not.toContainText(product[0].name);
+        await expect(cartProductRow.locator('.cart_price p')).not.toContainText(product[0].price);
+        await expect(cartProductRow.locator('.cart_quantity button')).not.toContainText(product[0].quantity);
     }
 
     async verifyProductQuantityInCart(row: number, expectedQuantity: string){
         const productInCartRow = this.getCartProductRow(row);
         await expect(productInCartRow.locator('.cart_quantity button')).toContainText(expectedQuantity);
+    }
+
+    async removeProductFromCart(row: number){
+        const productToRemove = this.getCartProductRow(row);
+        await (productToRemove.locator('.cart_quantity_delete')).click();
     }
 
     constructor(private page: Page){
